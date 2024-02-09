eaf0ccd077f8e97e38511dcfda272d32522d64b7
ksksback <madam.marsianovna@yandex.ru>
2024-02-09 12:52:49 +0300

    add UITabBarController

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Ксения Митранова on 02.02.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private var Avatar: UIImageView!
+    @IBOutlet private var LoginName: UILabel!
+    @IBOutlet private var DescriptionLabel: UILabel!
+    @IBOutlet private var NameLabel: UILabel!
+    
+    @IBAction private func BackButton() {
+    }
+}
